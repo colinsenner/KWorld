16fe9d2db12c2b2701caaaedae8d7652ad243cd1
Colin Senner <colin.senner@gmail.com>
2021-07-24 17:25:21 -0700

    Make DriverTest now take a pid from command line

--- KWorld/DriverTest/DriverTest.cpp
+++ KWorld/DriverTest/DriverTest.cpp
@@ -8,27 +8,33 @@ int main(char argc, char** argv) {
   HANDLE device = INVALID_HANDLE_VALUE;
   BOOL status = FALSE;
   DWORD bytesReturned = 0;
-  CHAR inBuffer[128] = {0};
   CHAR outBuffer[128] = {0};
 
-  char message[] = "hello from userland";
+  if (argc < 2) {
+    printf("Usage: %s <pid>\n", argv[0]);
+    return 0;
+  }
 
-  RtlCopyMemory(inBuffer, message, strlen(message));
+  // PID to ask the driver to disable ThreadHideFromDebugger
+  size_t pid = atoi(argv[1]);
 
   device = CreateFileW(L"\\\\.\\KmdWorld", GENERIC_ALL, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM, 0);
 
   if (device == INVALID_HANDLE_VALUE) {
-    printf_s("> Could not open device: 0x%x\n", GetLastError());
-    return FALSE;
+    printf("[-] Could not open device: 0x%x\n", GetLastError());
+    return 0;
   }
 
-  printf_s("> Issuing IOCTL_THREAD_UNHIDE_FROM_DEBUGGER 0x%x\n", IOCTL_THREAD_UNHIDE_FROM_DEBUGGER);
-  status = DeviceIoControl(device, IOCTL_THREAD_UNHIDE_FROM_DEBUGGER, inBuffer, sizeof(inBuffer), outBuffer,
-                           sizeof(outBuffer), &bytesReturned, (LPOVERLAPPED)NULL);
-  printf_s("> IOCTL_SPOTLESS 0x%x issued\n", IOCTL_THREAD_UNHIDE_FROM_DEBUGGER);
-  printf_s("> Received from the kernel land: %s. Received buffer size: %d\n", outBuffer, bytesReturned);
+  printf("[+] Issuing IOCTL_THREAD_UNHIDE_FROM_DEBUGGER 0x%x for PID %zu\n", IOCTL_THREAD_UNHIDE_FROM_DEBUGGER, pid);
+  status = DeviceIoControl(device, IOCTL_THREAD_UNHIDE_FROM_DEBUGGER, &pid, sizeof(pid), outBuffer, sizeof(outBuffer),
+                           &bytesReturned, (LPOVERLAPPED)NULL);
+
+  if (status) {
+    printf("[+] Success\n");
+  } else {
+    printf("[-] Error: %d\n", GetLastError());
+  }
 
   CloseHandle(device);
-
-  system("Pause");
+  return 0;
 }
