bbfcd8e8c11bdf458084024a6dda2e5ce5466c07
Colin Senner <colin.senner@gmail.com>
2021-07-24 17:24:48 -0700

    Add common macros

--- KWorld/Common/DriverCommon.h
+++ KWorld/Common/DriverCommon.h
@@ -1,8 +1,14 @@
 #pragma once
 
 #define DRIVER_PREFIX "KmdWorld:"
-#define DRIVER_POOL_TAG 'WdmK'
 
 #define DbgPrintPrefix(s, ...) DbgPrint(DRIVER_PREFIX " " s "\n", __VA_ARGS__)
 
-#define IOCTL_THREAD_UNHIDE_FROM_DEBUGGER CTL_CODE(FILE_DEVICE_UNKNOWN, 0x2049, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_THREAD_UNHIDE_FROM_DEBUGGER \
+  CTL_CODE(FILE_DEVICE_UNKNOWN, 0x2049, METHOD_BUFFERED, FILE_ANY_ACCESS)
+
+#define ADDPTR(ptr, val) ((unsigned char*)ptr + val)
+#define SUBPTR(ptr, val) ((unsigned char*)ptr - val)
+
+#define CHECK_BIT(var, pos) ((var) & (1 << (pos)))
+#define CLEAR_BIT(p, n) ((p) &= ~((1) << (n)))
