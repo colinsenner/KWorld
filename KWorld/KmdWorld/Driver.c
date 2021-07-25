89640deb0606c33031563f22ccb117aba7a3c681
Colin Senner <colin.senner@gmail.com>
2021-07-24 17:26:31 -0700

    Driver now clears the HideFromDebugger on the pid threads

--- KWorld/KmdWorld/Driver.c
+++ KWorld/KmdWorld/Driver.c
@@ -1,3 +1,4 @@
+#include <ntifs.h>
 #include <ntddk.h>
 #include <wdf.h>
 
@@ -8,6 +9,11 @@ DRIVER_INITIALIZE DriverEntry;
 UNICODE_STRING DEVICE_NAME = RTL_CONSTANT_STRING(L"\\Device\\KmdWorld");
 UNICODE_STRING DEVICE_SYMBOLIC_NAME = RTL_CONSTANT_STRING(L"\\??\\KmdWorld");
 
+//
+// Step 1: Pass pid to kernel and return success or failure
+// Step 2: Lookup process by pid
+// Step 3: Lookup structure offsets
+//
 
 void DriverUnload(PDRIVER_OBJECT DriverObject) {
   DbgPrintPrefix("Driver unloaded, deleting symbolic links and devices");
@@ -15,27 +21,87 @@ void DriverUnload(PDRIVER_OBJECT DriverObject) {
   IoDeleteSymbolicLink(&DEVICE_SYMBOLIC_NAME);
 }
 
+NTSTATUS ThreadUnhideFromDebugger(size_t pid) {
+  NTSTATUS status = STATUS_SUCCESS;
+
+  PEPROCESS pProcess = NULL;
+
+  status = PsLookupProcessByProcessId((HANDLE)pid, &pProcess);
+
+  if (status != STATUS_SUCCESS) {
+    DbgPrintPrefix("Couldn't find the process by pid %zu. Status: 0x%X", pid, status);
+    return status;
+  }
+
+  DbgPrintPrefix("Found process %p", pProcess);
+
+  ASSERT(pProcess);
+
+  // Calculate
+  PLIST_ENTRY pThreadListHead = (PLIST_ENTRY)ADDPTR(pProcess, 0x5e0);
+
+  ASSERT(pThreadListHead);
+
+  DbgPrintPrefix("ThreadListHead: %p", pThreadListHead);
+
+  PLIST_ENTRY entry = pThreadListHead;
+
+  while (entry != pThreadListHead->Flink) {
+    entry = entry->Flink;
+
+    // Calculate
+    PVOID pThread = SUBPTR(entry, 0x4e8);
+
+    ASSERT(pThread);
+
+    DbgPrintPrefix("  Thread: %p", pThread);
+
+    // offset: 0x510 on ETHREAD
+    PULONG CrossThreadFlags = (PULONG)ADDPTR(pThread, 0x510);
+
+    DbgPrintPrefix("    CrossThreadFlags: 0x%x", *CrossThreadFlags);
+
+    if (CHECK_BIT(*CrossThreadFlags, 2)) {
+      DbgPrintPrefix("    HideFromDebugger: Set");
+
+      // Unset HideFromDebugger
+      *CrossThreadFlags = CLEAR_BIT(*CrossThreadFlags, 2);
+
+      DbgPrintPrefix("    HideFromDebugger: Removed");
+    }
+  }
+
+  return status;
+}
+
 NTSTATUS HandleCustomIOCTL(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
   UNREFERENCED_PARAMETER(DeviceObject);
   PIO_STACK_LOCATION stackLocation = NULL;
-  CHAR *messageFromKernel = "ohai from them kernelz";
+
+  NTSTATUS status = STATUS_SUCCESS;
 
   stackLocation = IoGetCurrentIrpStackLocation(Irp);
 
   if (stackLocation->Parameters.DeviceIoControl.IoControlCode == IOCTL_THREAD_UNHIDE_FROM_DEBUGGER) {
-    DbgPrintPrefix("IOCTL_THREAD_UNHIDE_FROM_DEBUGGER (0x%x) issued", stackLocation->Parameters.DeviceIoControl.IoControlCode);
-    DbgPrintPrefix("Input received from userland: %s", (char *)Irp->AssociatedIrp.SystemBuffer);
+    DbgPrintPrefix("IOCTL_THREAD_UNHIDE_FROM_DEBUGGER (0x%x) issued",
+                   stackLocation->Parameters.DeviceIoControl.IoControlCode);
+
+    ASSERT(stackLocation->Parameters.DeviceIoControl.InputBufferLength == sizeof(size_t));
+    ASSERT(Irp->AssociatedIrp.SystemBuffer);
+
+    size_t pid = *(size_t*)Irp->AssociatedIrp.SystemBuffer;
+    DbgPrintPrefix("PID from userland: %zu", pid);
+
+    status = ThreadUnhideFromDebugger(pid);
   }
 
-  Irp->IoStatus.Information = strlen(messageFromKernel);
-  Irp->IoStatus.Status = STATUS_SUCCESS;
-
-  DbgPrintPrefix("Sending to userland: %s", messageFromKernel);
-  RtlCopyMemory(Irp->AssociatedIrp.SystemBuffer, messageFromKernel, strlen(Irp->AssociatedIrp.SystemBuffer));
+  // How many bytes we're returning to userland
+  Irp->IoStatus.Information = 0;
+  Irp->IoStatus.Status = status;
 
   IoCompleteRequest(Irp, IO_NO_INCREMENT);
 
-  return STATUS_SUCCESS;
+  return status;
 }
 
 NTSTATUS HandleIrpCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
@@ -61,7 +127,6 @@ NTSTATUS HandleIrpCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
 
   return STATUS_SUCCESS;
 }
-
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
   UNREFERENCED_PARAMETER(DriverObject);
