15ecd95b59eacdea5222547595a6009c37dc9b91
Colin Senner <colin.senner@gmail.com>
2021-08-04 23:38:30 -0700

    Port TrackProcesses

---
+++ KWorld/KmdWorld/TrackProcesses.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <ntddk.h>
+
+typedef struct _THREAD_INFO {
+  LIST_ENTRY ThreadEntry;
+  HANDLE tid;
+} THREAD_INFO, *PTHREAD_INFO;
+
+typedef struct _PROCESS_INFO {
+  LIST_ENTRY ProcessEntry;
+  HANDLE pid;
+  LIST_ENTRY ThreadListHead;
+} PROCESS_INFO, *PPROCESS_INFO;
+
+void InitializeTrackedProcesses();
+
+PPROCESS_INFO AddProcess(HANDLE pid);
+
+PTHREAD_INFO AddThreadToProcess(HANDLE pid, HANDLE tid);
+
+BOOLEAN RemoveProcess(HANDLE pid);
+
+BOOLEAN RemoveThreadFromProcess(HANDLE pid, HANDLE tid);
+
+void FreeTrackedProcesses();
+
+BOOLEAN IsProcessTracked(HANDLE pid);
+
+// Debug
+void PrintProcessList();
