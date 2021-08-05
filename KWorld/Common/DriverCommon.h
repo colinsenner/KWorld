7e51944b09d033e3eaedd5fad0f46f202289664e
Colin Senner <colin.senner@gmail.com>
2021-08-04 21:09:49 -0700

    Change DbgPrint to DbgPrintEx per msft's instructions

--- KWorld/Common/DriverCommon.h
+++ KWorld/Common/DriverCommon.h
@@ -3,7 +3,9 @@
 #define DRIVER_PREFIX "KmdWorld:"
 #define DRIVER_POOL_TAG 'WdmK'
 
-#define DbgPrintPrefix(s, ...) DbgPrint(DRIVER_PREFIX " " s "\n", __VA_ARGS__)
+#define DbgPrintPrefix(s, ...)                          \
+  DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_TRACE_LEVEL, \
+             DRIVER_PREFIX " " s "\n", __VA_ARGS__)
 
 #define IOCTL_THREAD_UNHIDE_FROM_DEBUGGER \
   CTL_CODE(FILE_DEVICE_UNKNOWN, 0x2049, METHOD_BUFFERED, FILE_ANY_ACCESS)
