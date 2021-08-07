c5de03db7272817f75889cad48b023e0e7c75d94
Colin Senner <colin.senner@gmail.com>
2021-08-07 15:48:11 -0700

    Modify NoBreakpointsAllowed to spawn another non-protected thread

--- KWorld/NoBreakpointsAllowed/NoBreakpointsAllowed.cpp
+++ KWorld/NoBreakpointsAllowed/NoBreakpointsAllowed.cpp
@@ -1,12 +1,23 @@
 #include <Windows.h>
 #include <stdio.h>
+#include <thread>
 
 #include "ThreadHideFromDebugger.h"
+
+void thread_two() {
+  printf("[+] New thread created: %d (0x%X)\n", GetCurrentThreadId(), GetCurrentThreadId());
+
+  while (true) {
+    Sleep(1000);
+  }
+}
 
 int main() {
   printf("[+] Starting\n");
   printf("[+] PID: %d (0x%X)\n", GetCurrentProcessId(), GetCurrentProcessId());
   printf("[+] Main thread id: %d (0x%X)\n", GetCurrentThreadId(), GetCurrentThreadId());
+
+  std::thread th2 (thread_two);
 
   if (ThreadHideFromDebugger::Enable()) {
     printf("[+] Thread hidden from debugger\n");
