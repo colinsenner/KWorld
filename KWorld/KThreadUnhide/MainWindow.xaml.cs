addb881bba1a106e6d4b06651e27c3469870c59d
Colin Senner <colin.senner@gmail.com>
2021-08-09 22:59:59 -0700

    Change to datagrid

--- KWorld/KThreadUnhide/MainWindow.xaml.cs
+++ KWorld/KThreadUnhide/MainWindow.xaml.cs
@@ -2,17 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Diagnostics;
+
+// TODO Automatically filter out system processes
+// TODO Add checkbox to enable viewing of system processes
 
 namespace KThreadUnhide
 {
@@ -32,10 +26,10 @@ namespace KThreadUnhide
 
         public void PopulateProcesses()
         {
-            processListView.ItemsSource = null;
-            processListView.Items.Clear();
+            processDataGrid.ItemsSource = null;
+            processDataGrid.Items.Clear();
 
-            processListView.ItemsSource = Process.GetProcesses();
+            processDataGrid.ItemsSource = Process.GetProcesses();
         }
 
         private void refreshButton_Click(object sender, RoutedEventArgs e)
@@ -46,28 +40,6 @@ namespace KThreadUnhide
         private void unhideButton_Click(object sender, RoutedEventArgs e)
         {
 
-        }
-
-        private void ListView_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            UpdateColumnsWidth(sender as ListView);
-        }
-
-        private void ListView_Loaded(object sender, RoutedEventArgs e)
-        {
-            UpdateColumnsWidth(sender as ListView);
-        }
-
-        private void UpdateColumnsWidth(ListView listView)
-        {
-            int autoFillColumnIndex = (listView.View as GridView).Columns.Count - 1;
-            if (listView.ActualWidth == double.NaN)
-                listView.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
-            double remainingSpace = listView.ActualWidth - 28;
-            for (int i = 0; i < (listView.View as GridView).Columns.Count; i++)
-                if (i != autoFillColumnIndex)
-                    remainingSpace -= (listView.View as GridView).Columns[i].ActualWidth;
-            (listView.View as GridView).Columns[autoFillColumnIndex].Width = remainingSpace >= 0 ? remainingSpace : 0;
         }
     }
 }
