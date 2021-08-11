a64c2e5157acb2528670f485808f25d72d946516
Colin Senner <colin.senner@gmail.com>
2021-08-10 20:11:41 -0700

    User settings implemented

--- KWorld/KThreadUnhide/App.xaml.cs
+++ KWorld/KThreadUnhide/App.xaml.cs
@@ -13,5 +13,9 @@ namespace KThreadUnhide
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            Settings.Default.Save();
+        }
     }
 }
